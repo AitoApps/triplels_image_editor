20907f696625975a54fed2ea94711d1497ffda28
Jeffrey <jeffrey950310@gmail.com>
2018-12-09 13:01:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# triplels_photo_editor
+
+This is a Flutter app that I wrote for my godmom actually, to add her brand logo and some texts on her product photos before uploading to social media.
+
+The reason I decide to create an app for her is because other image editing apps are too overwhelming and complicated to her. Another reason is an excuse for me to learn Flutter :P.
+
+This is a work in progress.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'edit_image.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'TripleLS Photo Editor',
+      theme: new ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: new HomePage(title: 'Select image'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      floatingActionButton: new FloatingActionButton.extended(
+        onPressed: () async {
+          var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+          if (image != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EditImagePage(title: 'Edit image', image: image),
+              ),
+            );
+          }
+        },
+        label: Text('Select image'),
+        icon: Icon(Icons.add_a_photo),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
